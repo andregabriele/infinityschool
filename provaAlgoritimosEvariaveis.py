afa0f97a21253d5999a4df579680c2ec836ea16a
André Luiz Teixeira Fiusa Gabriele <83142066+andregabriele@users.noreply.github.com>
2025-06-13 23:41:15 -0300

    Add files via upload

---
+++ provaAlgoritimosEvariaveis.py
@@ -0,0 +1,9 @@
+
+#Infinity School - Prova Algoritimos e
+
+base_retangulo = float(input("Digite o valor da Base do Retangulo: "))
+altura_retangulo = float(input("Digite o valor da Altura do Retangulo: "))
+
+area_retangulo = base_retangulo * altura_retangulo
+
+print(f"A área calculada do retângulo é: {area_retangulo}")  
