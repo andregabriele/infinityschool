eaf1616447545de83a154ddde36ca6b41d8bd138
André Luiz Teixeira Fiusa Gabriele <83142066+andregabriele@users.noreply.github.com>
2025-06-17 22:57:49 -0300

    Add files via upload

---
+++ provaCondicionais.py
@@ -0,0 +1,8 @@
+numero = float(input("Insira um número: "))
+
+if numero > 0:
+    print(f"O número é positivo. Número inserido pelo usuário: {numero}")
+elif numero == 0:
+    print(f"O número é zero. NNúmero inserido pelo usuário: {numero}")
+else:
+    print(f"O número é negativo. Número inserido pelo usuário: {numero}")
